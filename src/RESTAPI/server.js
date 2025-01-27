d7a3a2dd1cae21911a3b90b1699960a3c05817a6
Your Name <you@example.com>
2025-01-27 14:16:37 +0100

    RestAPI Finished

--- src/RESTAPI/server.js
+++ src/RESTAPI/server.js
@@ -1,53 +1,44 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-
+const express = require("express")
+const bodyParser = require('body-parser');
+ 
 const app = express();
-const port = 3000;
-
-let persons = [
-  {
-    id: 1,
-    firstname: "John",
-    lastname: "Doe",
-    age: 25,
-  },
-  {
-    id: 2,
-    firstname: "Jane",
-    lastname: "Smith",
-    age: 30,
-  },
-  {
-    id: 3,
-    firstname: "Michael",
-    lastname: "Johnson",
-    age: 35,
-  },
-  {
-    id: 4,
-    firstname: "Emily",
-    lastname: "Davis",
-    age: 28,
-  },
-  {
-    id: 5,
-    firstname: "Chris",
-    lastname: "Brown",
-    age: 40,
-  },
-];
-
-app.use(cors());
-
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json());
+ 
+const port = 3000;
+ 
+const data = [
+    { name: "Ahmet", age: 17},
+    { name: "Eyüp", age: 17},
+    { name: "Sam", age: 17},
+    { name: "Talha", age: 17},
+];
+ 
+app.get("/people",(req, res)=>{
+    res.send(data);
+})
 
-app.get("/", (req, res) => {
-  res.send("Hello World, from Trabzon");
-});
-
-
-app.listen(port, () =>
-  console.log(`Hello world app listening on port ${port}!`)
-);
+app.get("/people/:id",(req, res)=>{
+    let id = req.params.id;
+    res.send(data[id]);
+})
+ 
+app.delete("/people/:id", (req,res)=>{
+    let id = req.params.id;
+    data.splice(id,1);
+    res.send("done");
+})
+ 
+app.post("/people",(req, res)=>{
+    data.push(req.body);
+ 
+    res.send(req.body);
+})
+ 
+app.get("/",(req, res)=>{
+    res.send("Ahmet is Here");
+})
+ 
+app.listen(port,()=>{
+    console.log("Server running on port " + port)
+})
