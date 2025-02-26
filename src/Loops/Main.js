6a60c1428c357759ef8b5e4dc37ec50630c27202
Your Name <you@example.com>
2025-02-26 13:00:32 +0100

    Loops Aufgabe 9

--- src/Loops/Main.js
+++ src/Loops/Main.js
@@ -1,6 +1,7 @@
 sum = 1;
-for (let i = 1; i <= 5; i++) {
-    sum *= i
+for (let i = 1; i <= 10; i++) {
+    result = i * 3;
+    zahl = i;
+    console.log(zahl + " * 3 = " + result);
     
 }
-console.log(sum);
