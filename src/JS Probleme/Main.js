265b3e054dfb3a9023ea66fabe89327f0da1c944
Your Name <you@example.com>
2025-03-05 12:25:14 +0100

    Javascript Probleme

---
+++ src/JS Probleme/Main.js
@@ -0,0 +1,12 @@
+//erstes proiblem java wandelt typen automatisch um
+console.log('5' - 2);
+console.log('5' + 2);
+//zweites Problem JavaScript (wie viele andere Programmiersprachen) nutzt für Zahlen das IEEE 754 Standard-Format (64-bit Floating Point). 
+// Dadurch können bestimmte Dezimalzahlen nicht exakt dargestellt werden, weil sie im Binärsystem unendlich wären.
+var result = 0.1 + 0.2;
+console.log(result);
+
+
+//Größten Probleme:
+// Der Quellcode ist für alle zugänglich, dass heißt dass es Sicherheitsrisikos beinhaltet, 
+// da man in den Quellcode Viren oder Schadsoftware hineinschmuggeln kann ohne dass der Webseitenbesitzer es bemerkt.
